fde3adc4ad09227b49c5d6b6e0cb30f45af49725
Steven Arbucci <sg1strgt@gmail.com>
2026-08-18 18:43:17 -0500

    Remove run history features

---
+++ supabase/migrations/20260818010000_remove_run_history.sql
@@ -0,0 +1,4 @@
+drop table if exists public.run_history_entries cascade;
+drop table if exists public.run_history_day_settings cascade;
+drop table if exists public.run_history_hidden_ranges cascade;
+drop table if exists public.run_history_drivers cascade;
